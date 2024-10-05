97f60578c1b09e7d6e91a4a3fbfbb2f41c313acb
Jorge Eduardo Melgarejo Castillo <jorge.melgarejo@utec.edu.pe>
2024-10-05 09:11:35 -0500

    m

--- tienda-api-typescript/src/database.ts
+++ tienda-api-typescript/src/database.ts
@@ -3,8 +3,9 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 // Replace `http://localhost:8000` with the endpoint of your DynamoDB instance
 const client = new DynamoDBClient({
-  region: "us-east-1", // Adjust region if necessary
-  endpoint: "http://localhost:8000", // Replace with the IP and Port of your DynamoDB server
+  region: "local", // Adjust region if necessary
+  endpoint: "http://54.236.88.237:8000", // Replace with the IP and Port of your DynamoDB server
+    credentials: { accessKeyId: "fakeMyKeyId", secretAccessKey: "fakeSecretAccessKey" }, // Credenciales falsas para evitar errores
 });
 export const dynamoDB = DynamoDBDocumentClient.from(client);
 
