0a80f166eb797534d94bd5e81933c2f83b26c225
Jorge Eduardo Melgarejo Castillo <166084525+joruughen@users.noreply.github.com>
2024-10-05 22:20:32 -0500

    Update database.ts

--- tienda-api-typescript/src/database.ts
+++ tienda-api-typescript/src/database.ts
@@ -4,7 +4,7 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 // Replace `http://localhost:8000` with the endpoint of your DynamoDB instance
 const client = new DynamoDBClient({
   region: "local", // Adjust region if necessary
-  endpoint: "http://54.236.88.237:8000", // Replace with the IP and Port of your DynamoDB server
+  endpoint: "http://3.230.28.178:8000", // Replace with the IP and Port of your DynamoDB server
     credentials: { accessKeyId: "fakeMyKeyId", secretAccessKey: "fakeSecretAccessKey" }, // Credenciales falsas para evitar errores
 });
 export const dynamoDB = DynamoDBDocumentClient.from(client);
